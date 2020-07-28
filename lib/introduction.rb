95fa86d2c396641763cbe5f8a8a81fd35c16e4c6
Christie Yoon <cleex1999@gmail.com>
2020-07-28 01:08:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,4 @@ def introduction_with_language(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
- 
+
